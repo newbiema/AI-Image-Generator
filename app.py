01f64e6f2880a50e8cb09906573ac6cb8fcb95af
Evan Jamaq <xevan19@gmail.com>
2024-08-20 21:54:47 +0800

    edit path

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import replicate
 from dotenv import load_dotenv
 import os
 
-# Memuat variabel lingkungan dari .env file
+
 load_dotenv()
 
 app = Flask(__name__)
 
-# Ambil API token dari variabel lingkungan
+
 api_token = os.getenv("REPLICATE_API_TOKEN")
 client = replicate.Client(api_token=api_token)
 
@@ -26,7 +26,7 @@ def generate_image():
             "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
             input={"prompt": prompt}
         )
-        print("Output dari API:", output)  # Debug print statement
+        print("Output dari API:", output)  
     except Exception as e:
         print("Error:", e)
         return jsonify({"error": str(e)}), 500
